9495f4cb6daa08da31b7b29f6f9d27ff65976111
Thomas Nilsson <tnni@dtu.dk>
2018-12-28 23:04:56 +0100

    04

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ml02450',
+    version='0.0.4',
+    packages=[''],
+    url='https://github.com/thomasnilsson/ml02450',
+    license='MIT',
+    author='tnni',
+    author_email='tnni@dtu.dk',
+    description='A library for the machine learning and data mining course at DTU'
+)
